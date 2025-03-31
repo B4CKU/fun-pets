d10398438f1d6da71e136ee1a363becc588f07d6
B4CKU <szymekstrojny@gmail.com>
2025-03-31 08:05:05 +0200

    first commit

---
+++ src/client/java/com/zhaba/funpets/FunPetsClient.java
@@ -0,0 +1,10 @@
+package com.zhaba.funpets;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FunPetsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
